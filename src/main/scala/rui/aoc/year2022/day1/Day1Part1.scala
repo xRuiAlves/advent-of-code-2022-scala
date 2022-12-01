84c58f9b1e8c25e043c885835487cc836776ea8c
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-01 21:41:25 +0000

    Add day 1 part 1

---
+++ src/main/scala/rui/aoc/year2022/day1/Day1Part1.scala
@@ -0,0 +1,16 @@
+package rui.aoc.year2022.day1
+
+import rui.aoc.year2022.ProblemSolution
+import rui.aoc.year2022.utils.FileIO
+
+class Day1Part1 extends ProblemSolution {
+  case class Status(max: Int, currSum: Int)
+
+  override def solve(): AnyVal = {
+    val inputLines = FileIO.readResourceLines("day1.txt")
+    inputLines.foldLeft(Status(0, 0))((status: Status, curr) => curr match {
+      case "" => Status(math.max(status.max, status.currSum), 0)
+      case _ => Status(status.max, status.currSum + curr.toInt)
+    }).max
+  }
+}
