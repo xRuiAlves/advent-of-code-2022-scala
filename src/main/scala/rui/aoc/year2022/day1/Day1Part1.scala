277719dbc42b273767a99de120401c6f49cabf1b
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-02 08:44:24 +0000

    Refactor day 1

--- src/main/scala/rui/aoc/year2022/day1/Day1Part1.scala
+++ src/main/scala/rui/aoc/year2022/day1/Day1Part1.scala
@@ -6,11 +6,11 @@ import rui.aoc.year2022.utils.FileIO
 class Day1Part1 extends ProblemSolution {
   case class Status(max: Int, currSum: Int)
 
-  override def solve(): AnyVal = {
-    val inputLines = FileIO.readResourceLines("day1.txt")
-    inputLines.foldLeft(Status(0, 0))((status: Status, curr) => curr match {
+  override def solve(): AnyVal = FileIO
+    .readResourceLines("day1.txt")
+    .foldLeft(Status(0, 0))((status: Status, curr) => curr match {
       case "" => Status(math.max(status.max, status.currSum), 0)
       case _  => Status(status.max, status.currSum + curr.toInt)
-    }).max
-  }
+    })
+    .max
 }
--- src/main/scala/rui/aoc/year2022/day1/Day1Part2.scala
+++ src/main/scala/rui/aoc/year2022/day1/Day1Part2.scala
@@ -6,17 +6,15 @@ import rui.aoc.year2022.utils.FileIO
 class Day1Part2 extends ProblemSolution {
   case class Status(list: List[Int], currSum: Int)
 
-  override def solve(): AnyVal = {
-    val inputLines = FileIO.readResourceLines("day1.txt")
-    inputLines
-      .foldLeft(Status(List.empty, 0))((status: Status, curr) => curr match {
-        case "" => Status(status.list :+ status.currSum, 0)
-        case _  => Status(status.list, status.currSum + curr.toInt)
-      })
-      .list
-      .sorted
-      .reverse
-      .take(3)
-      .sum
-  }
+  override def solve(): AnyVal = FileIO
+    .readResourceLines("day1.txt")
+    .foldLeft(Status(List.empty, 0))((status: Status, curr) => curr match {
+      case "" => Status(status.list :+ status.currSum, 0)
+      case _  => Status(status.list, status.currSum + curr.toInt)
+    })
+    .list
+    .sorted
+    .reverse
+    .take(3)
+    .sum
 }
