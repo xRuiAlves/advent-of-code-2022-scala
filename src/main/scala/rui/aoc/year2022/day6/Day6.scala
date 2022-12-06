0a3fb054b7637cdcf7a8086a8bcab50530c0eea4
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-06 09:31:23 +0000

    Refactor day 6

--- src/main/scala/rui/aoc/year2022/day6/Day6.scala
+++ src/main/scala/rui/aoc/year2022/day6/Day6.scala
@@ -1,15 +1,10 @@
 package rui.aoc.year2022.day6
 
 object Day6 {
-  def countCharsAtMarker(dataStream: String, markerSize: Int): Int = {
-    for (i <- 0 until dataStream.length - markerSize - 1) {
-      if (isMarker(dataStream.substring(i, i + markerSize), markerSize)) {
-        return i + markerSize
-      }
-    }
-
-    throw new Exception("Solution not found!")
-  }
-
-  def isMarker(marker: String, markerSize: Int): Boolean = marker.toSet.size == markerSize
+  def countCharsAtMarker(dataStream: String, markerSize: Int): Int = dataStream
+    .sliding(markerSize)
+    .zipWithIndex
+    .find(markerIndexPair => markerIndexPair._1.toSet.size == markerSize)
+    .map(_._2 + markerSize)
+    .get
 }
