ca1da18e6a780e934ad93554842114f8ef4df0e8
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-08 22:00:26 +0000

    Refactor day 8 part 1

--- src/main/scala/rui/aoc/year2022/day8/Day8Part1.scala
+++ src/main/scala/rui/aoc/year2022/day8/Day8Part1.scala
@@ -2,34 +2,14 @@ package rui.aoc.year2022.day8
 
 import rui.aoc.year2022.ProblemSolution
 import rui.aoc.year2022.day6.Day6.countCharsAtMarker
+import rui.aoc.year2022.day8.Day8.parseTreeGrid
 import rui.aoc.year2022.utils.FileIO
 
 import scala.collection.mutable
 
 class Day8Part1 extends ProblemSolution {
-
-  def findVisibleTrees(trees: mutable.Set[Tree], treeLine: Array[Tree]): Unit = treeLine.foldLeft(-1) {
-    case (maxHeight, tree) =>
-      if (tree.height > maxHeight) {
-        trees.addOne(tree)
-        tree.height
-      }
-      else maxHeight
-  }
-
   override def solve(): AnyVal = {
-    val grid: Array[Array[Tree]] = FileIO
-      .readResourceLines("day8.txt")
-      .zipWithIndex
-      .map {
-        case (lineStr, y) => lineStr
-          .toCharArray
-          .map(_ - '0')
-          .zipWithIndex
-          .map {
-            case (height, x) => Tree(height, x, y)
-          }
-      }
+    val grid: Array[Array[Tree]] = parseTreeGrid(FileIO.readResourceLines("day8.txt"))
     val trees = mutable.Set[Tree]()
 
     // Horizontal
@@ -47,5 +27,12 @@ class Day8Part1 extends ProblemSolution {
     trees.size
   }
 
-  case class Tree(height: Int, x: Int, y: Int)
+  def findVisibleTrees(trees: mutable.Set[Tree], treeLine: Array[Tree]): Unit = treeLine.foldLeft(-1) {
+    case (maxHeight, tree) =>
+      if (tree.height > maxHeight) {
+        trees.addOne(tree)
+        tree.height
+      }
+      else maxHeight
+  }
 }
