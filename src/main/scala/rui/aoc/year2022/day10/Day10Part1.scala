81b417761caaf34f66ef56b465a15258d404e48a
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-10 09:53:29 +0000

    Refactor day 10 part 1

--- src/main/scala/rui/aoc/year2022/day10/Day10Part1.scala
+++ src/main/scala/rui/aoc/year2022/day10/Day10Part1.scala
@@ -1,22 +1,17 @@
 package rui.aoc.year2022.day10
 
 import rui.aoc.year2022.ProblemSolution
+import rui.aoc.year2022.day10.Day10.computeCycleValues
 import rui.aoc.year2022.utils.FileIO
 
 import scala.collection.mutable
 
 class Day10Part1 extends ProblemSolution {
   override def solve(): AnyVal = {
-    val instructions = FileIO.readResourceLines("day10.txt")
-    val values = instructions.foldLeft(Vector(1))((values, instruction) => instruction match {
-      case "noop" => values.appended(values.last)
-      case _ => instruction.split(" ") match {
-        case Array(_, delta) => values.appended(values.last).appended(values.last + delta.toInt)
-      }
-    })
+    val cycleValues = computeCycleValues(FileIO.readResourceLines("day10.txt"))
 
-    (20 to  values.length by 40)
-      .map(i => i * values(i - 1))
+    (20 to  cycleValues.length by 40)
+      .map(i => i * cycleValues(i - 1))
       .sum
   }
 }
