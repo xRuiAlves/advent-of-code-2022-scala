cbdda9add53c530856c824f96a817a03fe193b3b
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-10 09:53:50 +0000

    Remove unused imports from day 10 part 1

--- src/main/scala/rui/aoc/year2022/day10/Day10Part1.scala
+++ src/main/scala/rui/aoc/year2022/day10/Day10Part1.scala
@@ -4,8 +4,6 @@ import rui.aoc.year2022.ProblemSolution
 import rui.aoc.year2022.day10.Day10.computeCycleValues
 import rui.aoc.year2022.utils.FileIO
 
-import scala.collection.mutable
-
 class Day10Part1 extends ProblemSolution {
   override def solve(): AnyVal = {
     val cycleValues = computeCycleValues(FileIO.readResourceLines("day10.txt"))
