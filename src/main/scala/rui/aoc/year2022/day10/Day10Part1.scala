25b5b100003a1d6bad45aed8ad5ddfe6462fcece
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-10 09:48:55 +0000

    Add day 10 part 1

---
+++ src/main/scala/rui/aoc/year2022/day10/Day10Part1.scala
@@ -0,0 +1,24 @@
+package rui.aoc.year2022.day10
+
+import rui.aoc.year2022.ProblemSolution
+import rui.aoc.year2022.utils.FileIO
+
+import scala.collection.mutable
+
+class Day10Part1 extends ProblemSolution {
+  override def solve(): AnyVal = {
+    val instructions = FileIO.readResourceLines("day10.txt")
+    val values = instructions.foldLeft(Vector(1))((values, instruction) => instruction match {
+      case "noop" => values.appended(values.last)
+      case _ => instruction.split(" ") match {
+        case Array(_, delta) => {
+          values.appended(values.last).appended(values.last + delta.toInt)
+        }
+      }
+    })
+
+    (20 to  values.length by 40)
+      .map(i => i * values(i - 1))
+      .sum
+  }
+}
