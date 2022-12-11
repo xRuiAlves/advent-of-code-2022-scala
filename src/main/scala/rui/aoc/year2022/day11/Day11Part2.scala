28a3d6071ef1080e81c52859a240d8d3f5476403
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-11 09:07:44 +0000

    Refactor day 11 part 2

--- src/main/scala/rui/aoc/year2022/day11/Day11Part2.scala
+++ src/main/scala/rui/aoc/year2022/day11/Day11Part2.scala
@@ -2,17 +2,15 @@ package rui.aoc.year2022.day11
 
 import rui.aoc.year2022.ProblemSolution
 import rui.aoc.year2022.day10.Day10.computeCycleValues
+import rui.aoc.year2022.day11.Day11.{applyOp, parseMonkeys}
 import rui.aoc.year2022.utils.FileIO
 
 import scala.collection.mutable.ArrayBuffer
 
 class Day11Part2 extends ProblemSolution {
   override def solve(): AnyVal = {
-    val monkeys = FileIO
-      .readResourceLines("day11.txt")
-      .grouped(7)
-      .toArray
-      .map(parseMonkey)
+    val monkeys = parseMonkeys(FileIO
+      .readResourceLines("day11.txt"))
 
     val divisorsLcm = monkeys
       .map(_.divTestDelta)
@@ -26,28 +24,6 @@ class Day11Part2 extends ProblemSolution {
       .map(_.toLong)
       .takeRight(2)
       .product
-  }
-
-  def parseMonkey(monkeyInput: Array[String]): Monkey = {
-    val items = monkeyInput(1)
-      .substring(18)
-      .split(", ")
-      .map(_.toLong)
-      .to(ArrayBuffer)
-    val operand = monkeyInput(2)(23)
-    val operationDelta = monkeyInput(2).substring(25)
-    val divTestDelta = monkeyInput(3).substring(21).toInt
-    val trueTargetMonkey = monkeyInput(4).substring(29).toInt
-    val falseTargetMonkey = monkeyInput(5).substring(30).toInt
-
-    Monkey(items, operand, operationDelta, divTestDelta, trueTargetMonkey, falseTargetMonkey, 0)
-  }
-
-  def applyOp(item: Long, operand: Char, operationDelta: String): Long = (operand, operationDelta) match {
-    case ('+', "old") => item + item
-    case ('*', "old") => item * item
-    case ('+', delta) => item + delta.toInt
-    case ('*', delta) => item * delta.toInt
   }
 
   def processRound(monkeys: Array[Monkey], divisorsLcm: Int): Unit = {
