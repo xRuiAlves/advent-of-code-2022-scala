50a075fd1abad909fccadaa6be9bd596059612fc
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-12 10:06:20 +0000

    Refactor day 11 part 2

--- src/main/scala/rui/aoc/year2022/day11/Day11Part2.scala
+++ src/main/scala/rui/aoc/year2022/day11/Day11Part2.scala
@@ -1,42 +1,17 @@
 package rui.aoc.year2022.day11
 
 import rui.aoc.year2022.ProblemSolution
-import rui.aoc.year2022.day11.Day11.{applyOp, parseMonkeys}
+import rui.aoc.year2022.day11.Day11.{parseMonkeys, processRounds}
 import rui.aoc.year2022.utils.FileIO
-
 
 class Day11Part2 extends ProblemSolution {
   override def solve(): AnyVal = {
-    val monkeys = parseMonkeys(FileIO
-      .readResourceLines("day11.txt"))
-
+    val monkeys = parseMonkeys(FileIO.readResourceLines("day11.txt"))
     val divisorsLcm = monkeys
       .map(_.divTestDelta)
       .product
+    def modifyWorryLevel(curr: Long): Long = curr % divisorsLcm
 
-    (0 until 10000).foreach(_ => processRound(monkeys, divisorsLcm))
-
-    monkeys
-      .map(_.numItemsInspected)
-      .sorted
-      .map(_.toLong)
-      .takeRight(2)
-      .product
-  }
-
-  def processRound(monkeys: Array[Monkey], divisorsLcm: Int): Unit = {
-    monkeys.foreach(monkey => {
-      monkey.items.foreach(item => {
-        val valueAfterOp = applyOp(item, monkey.operand, monkey.operationDelta)
-        val boredVal = valueAfterOp % divisorsLcm
-        if (boredVal % monkey.divTestDelta == 0) {
-          monkeys(monkey.trueTargetMonkey).items.addOne(boredVal)
-        } else {
-          monkeys(monkey.falseTargetMonkey).items.addOne(boredVal)
-        }
-        monkey.numItemsInspected += 1
-      })
-      monkey.items.clear()
-    })
+    processRounds(monkeys, 10000, modifyWorryLevel)
   }
 }
