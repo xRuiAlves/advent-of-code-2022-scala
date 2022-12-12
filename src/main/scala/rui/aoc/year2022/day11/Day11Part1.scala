0fb1a11d33363321f533782cfc9c710c0c5c020a
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-12 09:56:58 +0000

    Cleanup day 11 part 1

--- src/main/scala/rui/aoc/year2022/day11/Day11Part1.scala
+++ src/main/scala/rui/aoc/year2022/day11/Day11Part1.scala
@@ -4,8 +4,6 @@ import rui.aoc.year2022.ProblemSolution
 import rui.aoc.year2022.day11.Day11.{applyOp, parseMonkeys}
 import rui.aoc.year2022.utils.FileIO
 
-import scala.collection.mutable.ArrayBuffer
-
 class Day11Part1 extends ProblemSolution {
   override def solve(): AnyVal = {
     val monkeys = parseMonkeys(FileIO
@@ -19,21 +17,6 @@ class Day11Part1 extends ProblemSolution {
       .map(_.toLong)
       .takeRight(2)
       .product
-  }
-
-  def parseMonkey(monkeyInput: Array[String]): Monkey = {
-    val items = monkeyInput(1)
-      .substring(18)
-      .split(", ")
-      .map(_.toLong)
-      .to(ArrayBuffer)
-    val operand = monkeyInput(2)(23)
-    val operationDelta = monkeyInput(2).substring(25)
-    val divTestDelta = monkeyInput(3).substring(21).toInt
-    val trueTargetMonkey = monkeyInput(4).substring(29).toInt
-    val falseTargetMonkey = monkeyInput(5).substring(30).toInt
-
-    Monkey(items, operand, operationDelta, divTestDelta, trueTargetMonkey, falseTargetMonkey, 0)
   }
 
   def processRound(monkeys: Array[Monkey]): Unit = {
