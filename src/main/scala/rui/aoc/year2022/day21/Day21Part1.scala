f9e0f00f34263a91e3642308d58fd1745d38528e
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-23 08:43:09 +0000

    Refactor day 21 part 1

--- src/main/scala/rui/aoc/year2022/day21/Day21Part1.scala
+++ src/main/scala/rui/aoc/year2022/day21/Day21Part1.scala
@@ -8,41 +8,30 @@ import scala.collection.mutable
 class Day21Part1 extends ProblemSolution {
   private final val ROOT_MONKEY_NAME = "root"
 
-  override def solve(): AnyVal = {
-    val monkeys = FileIO
-      .readResourceLines("day21.txt")
-      .map(Monkey.apply)
-      .map(monkey => monkey.name -> monkey)
-      .toMap
-
-    val cache = mutable.Map[String, Long]()
-    monkeyYellValue(monkeys, cache, ROOT_MONKEY_NAME)
-  }
+  override def solve(): AnyVal = monkeyYellValue(FileIO
+    .readResourceLines("day21.txt")
+    .map(Monkey.apply)
+    .map(monkey => monkey.name -> monkey)
+    .toMap, ROOT_MONKEY_NAME)
 
   def monkeyYellValue(
     monkeys: Map[String, Monkey],
-    cache: mutable.Map[String, Long],
     monkeyName: String
-  ): Long = cache.get(monkeyName) match {
-    case Some(cachedValue) => cachedValue
-    case None => {
-      val monkey = monkeys(monkeyName)
-      cache(monkeyName) = monkey.value match {
-        case Some(value) => value
-        case None => {
-          val monkeyOp = monkey.monkeyOp.get
-          val leftYellValue = monkeyYellValue(monkeys, cache, monkeyOp.leftMonkeyName)
-          val rightYellValue = monkeyYellValue(monkeys, cache, monkeyOp.rightMonkeyName)
-          monkeyOp.op match {
-            case '+' => leftYellValue + rightYellValue
-            case '-' => leftYellValue - rightYellValue
-            case '*' => leftYellValue * rightYellValue
-            case '/' => leftYellValue / rightYellValue
-          }
+  ): Long = {
+    val monkey = monkeys(monkeyName)
+    monkey.value match {
+      case Some(value) => value
+      case None => {
+        val monkeyOp = monkey.monkeyOp.get
+        val leftYellValue = monkeyYellValue(monkeys, monkeyOp.leftMonkeyName)
+        val rightYellValue = monkeyYellValue(monkeys, monkeyOp.rightMonkeyName)
+        monkeyOp.op match {
+          case '+' => leftYellValue + rightYellValue
+          case '-' => leftYellValue - rightYellValue
+          case '*' => leftYellValue * rightYellValue
+          case '/' => leftYellValue / rightYellValue
         }
       }
-      cache(monkeyName)
     }
   }
-
 }
