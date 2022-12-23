241857677ce923e51f5c5a4919d4d86c0b1739cc
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-23 15:30:11 +0000

    Refactor day 21 part 1

--- src/main/scala/rui/aoc/year2022/day21/Day21Part1.scala
+++ src/main/scala/rui/aoc/year2022/day21/Day21Part1.scala
@@ -1,37 +1,13 @@
 package rui.aoc.year2022.day21
 
 import rui.aoc.year2022.ProblemSolution
+import rui.aoc.year2022.day21.Day21._
 import rui.aoc.year2022.utils.FileIO
 
-import scala.collection.mutable
-
 class Day21Part1 extends ProblemSolution {
-  private final val ROOT_MONKEY_NAME = "root"
-
   override def solve(): AnyVal = monkeyYellValue(FileIO
     .readResourceLines("day21.txt")
     .map(Monkey.apply)
     .map(monkey => monkey.name -> monkey)
     .toMap, ROOT_MONKEY_NAME)
-
-  def monkeyYellValue(
-    monkeys: Map[String, Monkey],
-    monkeyName: String
-  ): Long = {
-    val monkey = monkeys(monkeyName)
-    monkey.value match {
-      case Some(value) => value
-      case None => {
-        val monkeyOp = monkey.monkeyOp.get
-        val leftYellValue = monkeyYellValue(monkeys, monkeyOp.leftMonkeyName)
-        val rightYellValue = monkeyYellValue(monkeys, monkeyOp.rightMonkeyName)
-        monkeyOp.op match {
-          case '+' => leftYellValue + rightYellValue
-          case '-' => leftYellValue - rightYellValue
-          case '*' => leftYellValue * rightYellValue
-          case '/' => leftYellValue / rightYellValue
-        }
-      }
-    }
-  }
 }
