4c65f528db70b54061f62a6f2a5ad9eb4c8d2d1c
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-12-23 15:26:05 +0000

    Refactor day 21 Monkey helper class

--- src/main/scala/rui/aoc/year2022/day21/Monkey.scala
+++ src/main/scala/rui/aoc/year2022/day21/Monkey.scala
@@ -2,13 +2,13 @@ package rui.aoc.year2022.day21
 
 case class Monkey(
   name: String,
-  value: Option[Int] = Option.empty,
+  value: Option[Long] = Option.empty,
   monkeyOp: Option[MonkeyOp] = Option.empty
 )
 
 object Monkey {
   def apply(rawMonkeyStr: String): Monkey = rawMonkeyStr.split(" ") match {
-    case Array(name, value) => Monkey(name.dropRight(1), value = Option(value.toInt))
+    case Array(name, value) => Monkey(name.dropRight(1), value = Option(value.toLong))
     case Array(name, leftMonkeyName, op, rightMonkeyName) =>
       Monkey(name.dropRight(1), monkeyOp = Option(MonkeyOp(leftMonkeyName, op.head, rightMonkeyName)))
   }
